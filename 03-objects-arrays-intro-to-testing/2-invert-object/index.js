6ee89e6e7b4d41ae457c487e86a0c31be05855fc
Artur Mingazhov <arthurming7@gmail.com>
2021-05-06 09:53:54 +0500

    add solution for 2-invert-object

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -5,4 +5,25 @@
  */
 export function invertObj(obj) {
 
+   if (!obj) return
+
+   const result = {};
+
+   for (const [key, value] of Object.entries(obj)) {
+
+      result[value] = key
+   }
+
+   return result
+
+   /*  if (!obj) return
+
+    return Object.entries(obj).reduce((result, [key, value]) => {
+
+       result[value] = key;
+
+       return result
+
+    }, {}) */
+
 }
