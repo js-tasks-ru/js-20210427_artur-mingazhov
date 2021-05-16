36cbadd962636db8ccdf5d48a0c6c96da6e2b19c
Artur Mingazhov <arthurming7@gmail.com>
2021-05-16 08:27:28 +0300

    add solution for 1-notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,63 @@
 export default class NotificationMessage {
 
+   static isActive = false;
+
+   constructor(message = '', { duration = 1000, type = "success" } = {}) {
+
+      this.message = message;
+      this.duration = duration;
+      this.type = type;
+      this.render();
+   }
+
+   get template() {
+      return `
+        <div class="notification ${this.type}" style="--value:${this.duration / 1000 + 's'}">
+          <div class="timer"></div>
+          <div class="inner-wrapper">
+            <div class="notification-header">${this.type}</div>
+            <div class="notification-body">
+              ${this.message}
+            </div>
+          </div>
+       </div>
+       `
+   }
+   render() {
+
+      const element = document.createElement('div');
+
+      element.innerHTML = this.template;
+
+      this.element = element.firstElementChild
+   }
+
+   show(elem = document.body) {
+
+      if (NotificationMessage.isActive) {
+
+         document.querySelector('.notification').remove();
+         this.destroy()
+      }
+
+      NotificationMessage.isActive = true;
+
+      setTimeout(() => {
+
+         NotificationMessage.isActive = false
+         this.destroy()
+      }, this.duration)
+
+      elem.append(this.element);
+   }
+
+   remove() {
+
+      this.element.remove();
+   }
+
+   destroy() {
+
+      this.remove();
+   }
 }
