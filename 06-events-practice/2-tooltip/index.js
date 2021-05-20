18c151d37025b45e6310952a7981b2bcb0f5aaf3
Artur Mingazhov <arthurming7@gmail.com>
2021-05-20 08:44:09 +0300

    add solution for tooltip

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,5 +1,69 @@
-class Tooltip {
+export default class Tooltip {
 
+   static #onlyInstance = null;
+
+   onPointerOver = (event) => {
+
+      const target = event.target.closest('[data-tooltip]');
+
+      if (!target) {
+         return
+      }
+
+      this.render(target.dataset.tooltip);
+
+      target.addEventListener('pointermove', this.onPointerMove)
+   }
+
+   onPointerOut = () => {
+      this.remove()
+   }
+
+   onPointerMove = (event) => {
+
+      this.element.style.left = event.clientX + 10 + 'px';
+
+      this.element.style.top = event.clientY + 10 + 'px';
+   }
+
+   constructor() {
+      if (!Tooltip.#onlyInstance) {
+         Tooltip.#onlyInstance = this;
+      } else {
+         return Tooltip.#onlyInstance;
+      }
+   }
+
+   render(elementText) {
+
+      const element = document.createElement('div');
+
+      element.innerHTML = `<div class="tooltip">${elementText}</div>`
+
+      this.element = element.firstElementChild
+
+      document.body.append(this.element)
+   }
+
+   initialize() {
+
+      document.body.addEventListener('pointerover', this.onPointerOver)
+
+      document.body.addEventListener('pointerout', this.onPointerOut)
+   }
+
+   remove() {
+      if (this.element)
+         this.element.remove()
+      //this.element = null
+   }
+
+   destroy() {
+      this.remove();
+      document.body.removeEventListener('pointerover', this.onPointerOver)
+      document.body.removeEventListener('pointerout', this.onPointerOut)
+      document.body.removeEventListener('pointermove', this.onPointerMove)
+   }
 }
 
-export default Tooltip;
+
